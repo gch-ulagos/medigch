6805ea8473e4d61d25990e9470662e7466d253e7
NoisyShadow <ilancamilovargas@gmail.com>
2024-04-07 20:00:07 -0400

    Seeder Base de datos
    Se modifica el seeder del proyecto para la creación de la base de datos.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +17,25 @@ class DatabaseSeeder extends Seeder
         $this->call(LaratrustSeeder::class);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (!Schema::hasTable('ordenes')) {
+            Schema::create('ordenes', function ($table) {
+                $table->id();
+                $table->foreignId('user_id')->constrained('users');
+                $table->foreignId('medic_id')->constrained('users');
+                $table->text('description');
+                $table->timestamps();
+            });
+        }
+
+        // Crear la tabla "examen" si no existe
+        if (!Schema::hasTable('examen')) {
+            Schema::create('examen', function ($table) {
+                $table->id();
+                $table->foreignId('order_id')->constrained('ordenes');
+                $table->string('archivo'); // Cambiar a tu tipo de dato específico para archivos
+                $table->timestamps();
+            });
+        }
     }
 }
+   
