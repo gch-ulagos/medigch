80418983bc940fb3db0767e4be903f0d7ae68daf
skarwinter <feiipe.rioz123@gmail.com>
2024-07-06 15:27:04 -0400

    index

---
+++ index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MediGCH</title>
+  <link href="{{ asset('css/index.css') }}" rel="stylesheet">  
+</head>
+<body>
+  <div class="container">
+    <h1>MediGCH</h1>
+    <a href="login" class="btn">Login</a>
+    <a href="register" class="btn">Registro</a>
+  </div>
+</body>
+</html>
